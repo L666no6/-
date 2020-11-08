2bfedfeb177a195db5c1ac002a50e8a23812dd7e
L666no6 <72982866+L666no6@users.noreply.github.com>
2020-11-08 13:50:01 +0800

    Add files via upload

---
+++ ASCLL.cpp
@@ -0,0 +1,16 @@
+//项目名:
+//编号:B-
+//制作人:林暄博
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char c;
+	for (int i=0;i<=128;i++)
+	{
+		c=(char)i;
+		printf ("%d\n%c\n\n",i,c);
+	}
+	return 0;
+}
+
